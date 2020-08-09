558a6672d7f9f440fc02121c472110c9366542b2
St4ckben <35505800+St4ckben@users.noreply.github.com>
2020-08-09 20:01:38 +0200

    Code cleaning

--- popstompbox.ino
+++ popstompbox.ino
@@ -14,81 +14,138 @@
 #include <SD.h>
 #include <EEPROM.h>
 
+#define DEBUG_ENABLED false
+#define DEBUG_PRINT(a) //(Serial.print(a))
+#define DEBUG_PRINTLN(a) //(Serial.println(a))
+
+#define PIN_PIEZO A0
+#define PIN_LED 8
+#define PIN_BUTTON 7
+#define PIN_SPEAKER 10
+
 #define EEADDR 166
+#define PIEZO_THRESHOLD 20
+#define TAP_REQUIRED_COUNT 3 // How many times the threshold must be consecutivly reached ?
+#define TAP_DELAY 80 // How much time should we wait until next tap ?
+#define VOLUME 2 // Must be 0 to 7
 
-const int PIEZO_PIN = A0; 
+// Sound selection vars
+int currentSound = 1;          // currently selected sound (the first one by default)
+File soundFile;                // The current opened sound file
 
-int currentSound = 1;
-File soundFile;
+// Audio vars
+TMRpcm audio;                  // Audio object
 
-TMRpcm audio;
-int piezoADC = 0;
-int threshold = 20;
-int previousPiezoADC = 0;
-int led = 8; 
-int button = 7; 
-int tapValidator = 0;
+// Piezo sensor vars
+int piezoValue = 0;            // Current piezo sensor value
+int previousPiezoValue = 0;    // Previous piezo sensor value
+int tapCounter = 0;            // Counter of continuous threshold detections
 
 void setup() {
-  // Led 
-  pinMode(led, OUTPUT); 
-  digitalWrite(led, LOW);
-
-  // Button 
-  pinMode(button,INPUT_PULLUP);
+  if (DEBUG_ENABLED) {
+    setupSerial();
+  }
+  setupLed();
+  setupButton();
+  setupSDCardReader();
+  setupAudioPlayer();
   
-  /*// Open serial communications and wait for port to open:
+  initSoundFile();  
+}
+
+void loop() {
+  processPiezoSensor();
+  checkButtonStatus();
+}
+
+// Setup methods
+/////////////////////////////////
+
+void setupSerial() {
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-  Serial.print("Initializing SD card...");*/
-
-  if (!SD.begin(4)) {
-    //Serial.println("initialization failed!");
-    while (1);
-  }
-  //Serial.println("initialization done.");
-
-  initSoundFile();  
- 
-  audio.speakerPin = 10;
-  audio.setVolume(2);
-
 }
 
-void loop() {
+void setupLed() {
+  DEBUG_PRINT("Initializing LED...");
+  pinMode(PIN_LED, OUTPUT); 
+  digitalWrite(PIN_LED, LOW); // Led must be enlighted when power is on
+  DEBUG_PRINTLN(" DONE.");
+}
 
-  piezoADC = analogRead(PIEZO_PIN);
-  //Serial.println(piezoADC); 
-  if (piezoADC >= (previousPiezoADC + threshold)) {
-    tapValidator++;
-    if (tapValidator >= 3) {
-      tapValidator = 0;
-      audio.play(soundFile.name());
-      //Serial.print("TAP !"); 
-      //Serial.println(piezoADC); 
-      delay(80);
-      previousPiezoADC = analogRead(PIEZO_PIN);
+void setupButton() {
+  DEBUG_PRINT("Initializing button...");
+  pinMode(PIN_BUTTON,INPUT_PULLUP); // The internal pullup resistor is used to connect the button
+  DEBUG_PRINTLN(" DONE.");
+}
+
+void setupSDCardReader() {
+  DEBUG_PRINT("Initializing SD card...");
+  if (!SD.begin(4)) {
+    DEBUG_PRINTLN(" FAILED.");
+    while (1);
+  }
+  DEBUG_PRINTLN(" DONE.");
+}
+
+void setupAudioPlayer() {
+  DEBUG_PRINT("Initializing audio player...");
+  audio.speakerPin = PIN_SPEAKER;
+  audio.setVolume(VOLUME);
+  DEBUG_PRINTLN(" DONE.");
+}
+
+void initSoundFile() {
+  DEBUG_PRINT("Loading initial sound file...");
+  EEPROM.get(EEADDR,currentSound); // Loading potentially existing sound number in EEPROM
+   
+  String filename = generateFilename();
+  if (!SD.exists(filename)) {
+    currentSound = 1;
+    filename = generateFilename();
+  }
+  soundFile = SD.open(filename, FILE_READ);
+  DEBUG_PRINT(" DONE (now using sound '");
+  DEBUG_PRINT(soundFile.name());
+  DEBUG_PRINTLN("').");
+}
+
+// Audio control methods
+//////////////////////////
+
+void processPiezoSensor() {
+  // Reading current piezo value
+  piezoValue = analogRead(PIN_PIEZO);
+
+  // If the current value exceed the last value + threshold
+  if (piezoValue >= (previousPiezoValue + PIEZO_THRESHOLD)) {
+    // The counter of threshold exceeded must be incremented. To minimize false positives, the sound is played when the threshold value is consecutivly exceeded several times (defined by TAP_REQUIRED_COUNT) 
+    tapCounter++;
+
+    // If the threshold has been exceeded more than defined by TAP_REQUIRED_COUNT 
+    if (tapCounter >= TAP_REQUIRED_COUNT) {
+      audio.play(soundFile.name()); // Let's play the sound
+      tapCounter = 0; // Set the counter back to 0
+      DEBUG_PRINT("TAP ! "); 
+      DEBUG_PRINTLN(piezoValue); 
+      delay(TAP_DELAY); // Wait a bit for the signal to go down 
+      previousPiezoValue = analogRead(PIN_PIEZO); // Get an actualized value of the piezo value for the next loop
     }
   } else {
-    tapValidator = 0;
-    previousPiezoADC = piezoADC;
+    tapCounter = 0; // If the threshold is not reached, reset the counter
+    previousPiezoValue = piezoValue; // Set the previous value for the next loop
   }
-
-  // If button is pressed
-  if (digitalRead(button)==LOW) {
-     changeSound();
-     delay(500);
-  } 
 }
 
 void changeSound() {
-  audio.disable(); 
-  soundFile.close();
+  audio.disable(); // First, let's disable the currently loaded sound file to prevent errors (sometimes it freezes).
+  soundFile.close(); // Closing the file in order not to overload memory
+  
   String filename;
-  while (!soundFile) {
+  while (!soundFile) { // While the sound file is not successfully open (can be infinite loop if no valid SOUND001.wav file on SD card).
     currentSound++;
     filename = generateFilename();
     if (!SD.exists(filename)) {
@@ -98,34 +155,35 @@ void changeSound() {
     soundFile = SD.open(filename, FILE_READ);
   }
   blinkLed();
-  EEPROM.put(EEADDR,currentSound);
-  //Serial.print("Now using sound ");
-  //Serial.println(soundFile.name());
+  EEPROM.put(EEADDR,currentSound); // Save the current choice to EEPROM
+  DEBUG_PRINT("Now using sound '");
+  DEBUG_PRINT(soundFile.name());
+  DEBUG_PRINTLN("'.");
 }
+
+// IHM methods
+//////////////////////////
 
 void blinkLed() {
-  digitalWrite(led, HIGH); // Eteinte
+  digitalWrite(PIN_LED, HIGH); // Off
   delay(80);
-  digitalWrite(led, LOW); // Allumée
+  digitalWrite(PIN_LED, LOW); // On
   delay(80);
-  digitalWrite(led, HIGH); // Eteinte
+  digitalWrite(PIN_LED, HIGH); // Off
   delay(80);
-  digitalWrite(led, LOW); // Allumée
+  digitalWrite(PIN_LED, LOW); // On
 }
 
-void initSoundFile() {
-  // Read EEPROM for stored sound value
-   EEPROM.get(EEADDR,currentSound);
-   
-  String filename = generateFilename();
-  if (!SD.exists(filename)) {
-    currentSound = 1;
-    filename = generateFilename();
-  }
-  soundFile = SD.open(filename, FILE_READ);
-  //Serial.print("Now using sound ");
-  //Serial.println(soundFile.name());
+void checkButtonStatus() {
+  // If button is pressed
+  if (digitalRead(PIN_BUTTON)==LOW) {
+     changeSound();
+     delay(500);
+  } 
 }
+
+// Helpers
+//////////////////////////
 
 String generateFilename() {
   String filename = "SOUND";
